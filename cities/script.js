930fbbb74780680c2fcda278a3e45bda25c141e0
Mark Adams <mark.adams82@gmail.com>
2024-04-14 17:58:31 +0100

    Cities Array experiment

---
+++ cities/script.js
@@ -0,0 +1,17 @@
+const cities = [
+  "Manchester",
+  "London",
+  "Birmingham",
+  "Leeds",
+  "Bristol",
+  "Liverpool",
+];
+const button = document.getElementById("button");
+const result = document.getElementById("result");
+button.addEventListener("click", randomise);
+
+function randomise() {
+  const random = Math.floor(Math.random() * cities.length);
+  let city = cities[random];
+  result.innerHTML = city;
+}
